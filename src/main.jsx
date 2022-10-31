373b8d3abece740a796fbc4d55f592c485625540
devsilva <jerson.silva99@hotmail.com>
2022-10-30 22:56:13 -0500

    feat: Upload initial files

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import { $ } from './utils/dom'
+import { store } from './redux/store'
+
+import './index.css'
+import App from './App'
+
+ReactDOM.createRoot($('#root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>
+)
